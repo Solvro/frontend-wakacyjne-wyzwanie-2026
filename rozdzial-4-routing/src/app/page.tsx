0ddb9c2ed85015cc7c794aca3435aed478103013
ShadowCatP <282276@student.pwr.edu.pl>
2026-08-11 18:26:07 +0200

    refactor: update main page with README directions

---
+++ rozdzial-4-routing/src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="text-center flex-col min-h-screen flex justify-center mx-auto max-w-5xl">
+      <h1 className="text-4xl font-bold mb-8">
+        Witamy w "Wakacyjnym wyzwaniu"
+      </h1>
+      <p className="text-gray-600 text-lg">
+        Sprawdź <span className="underline font-bold">README.md</span> i zobacz
+        co dla ciebie przygotowaliśmy
+      </p>
+    </main>
+  );
+}
